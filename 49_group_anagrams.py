f870c0c9b3a63adf36c9d8b4558841fa748bdd5e
apple <875172749@qq.com>
2018-11-09 15:32:32 +0800

    改进后击败99%

--- 49_group_anagrams.py
+++ 49_group_anagrams.py
@@ -8,31 +8,18 @@ class Solution(object):
         考虑用字典将不按字母表顺序排列的字母组合，转换成按照字母表排列的计数字典
         """
         result_dic = {}
-        for str_item in strs:
-            c_dic = {}
-            c_record_str = ""
-            for c in str_item:
-                if c_dic.has_key(c):
-                    c_dic[c] += 1
-                else:
-                    c_dic[c] = 1
-            # 字典的key排序有点迷，排下序
-            sorted_c_list = sorted(c_dic.items(), key=lambda x: x[0])
-            for sorted_c_item in sorted_c_list:
-                c_record_str += (sorted_c_item[0] + str(sorted_c_item[1]))
-
-            if result_dic.has_key(c_record_str):
-                result_dic[c_record_str].append(str_item)
+        for str in strs:
+            s_str = ''.join(sorted(str))
+            if s_str in result_dic:
+                result_dic[s_str].append(str)
             else:
-                result_dic[c_record_str] = [str_item]
-            print(str_item),
-            print(c_record_str)
+                result_dic[s_str] = [str]
 
         result = []
         for key in result_dic:
             result.append(result_dic[key])
 
-        print(result)
+        print result
 
 # =================================================================================
 
