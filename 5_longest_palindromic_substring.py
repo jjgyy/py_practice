541bb8b5f60e9d46c7aa455f670ac02d67d963d2
apple <875172749@qq.com>
2018-11-14 17:19:48 +0800

    完全没优化，有时间一定优化一定优化

--- 5_longest_palindromic_substring.py
+++ 5_longest_palindromic_substring.py
@@ -4,33 +4,28 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
+        s_length = len(s)
         max_length = 0
         longest_palindrome = ""
         ptrA = 0
-        ptrB = len(s) - 1
-        while True:
-            ptrB = len(s) - 1
-            if (ptrB - ptrA + 1) <= max_length:
-                break
-            while True:
-                if (ptrB - ptrA + 1) <= max_length:
-                    break
-                if self.is_palindromic(s[ptrA:ptrB+1]):
-                    longest_palindrome = s[ptrA:ptrB+1]
-                    max_length = ptrB - ptrA + 1
+        ptrB = s_length
+
+        while (ptrB - ptrA) > max_length:
+            while (ptrB - ptrA) > max_length:
+                temp = s[ptrA:ptrB]
+                if temp == temp[::-1]:
+                    longest_palindrome = temp
+                    max_length = ptrB - ptrA
                     break
                 ptrB -= 1
             ptrA += 1
+            ptrB = s_length
 
         return longest_palindrome
 
-    def is_palindromic(self, s):
-        if s == s[::-1]:
-            return True
-        return False
 
 
 # ==========================================
 solution = Solution()
-s = "aba"
+s = "cabac"
 print solution.longestPalindrome(s)
