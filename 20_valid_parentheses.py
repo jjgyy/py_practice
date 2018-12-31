285c5b5e163635e52c5912fc0e466b7a050c39a7
apple <875172749@qq.com>
2018-12-31 16:56:52 +0800

    蛮简单的

---
+++ 20_valid_parentheses.py
@@ -0,0 +1,49 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for c in s:
+            if c == '(':
+                stack.append(')')
+                continue
+            if c == '[':
+                stack.append(']')
+                continue
+            if c == '{':
+                stack.append('}')
+                continue
+
+            if c == ')':
+                if (len(stack) == 0) or stack[-1] != ')':
+                    return False
+                else:
+                    stack.pop()
+                    continue
+            if c == ']':
+                if (len(stack) == 0) or stack[-1] != ']':
+                    return False
+                else:
+                    stack.pop()
+                    continue
+            if c == '}':
+                if (len(stack) == 0) or (stack[-1] != '}'):
+                    return False
+                else:
+                    stack.pop()
+                    continue
+
+        if len(stack) != 0:
+            return False
+
+        return True
+
+
+
+
+# ========================================================
+solution = Solution()
+s = "[{()}]]"
+print solution.isValid(s)
