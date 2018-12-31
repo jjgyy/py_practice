2c24577b3be15b59f463cb9d149ea3a765e384e5
apple <875172749@qq.com>
2018-12-31 16:59:51 +0800

    给小谢的练习

---
+++ for_xieyingying/function.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def getLength(self, list):
+        a= 1
+        b = 2
+        length = len(list)
+        return list[0]
+
+
+
+solution = Solution()
+list = [1,2,3,4]
+
+
+
+
+print solution.getLength(list)
---
+++ for_xieyingying/string2int.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def string2int(self, string):
+        '''
+        :param string:
+        :return: int
+        '''
+
+
+
+
+    def int2string(self, int):
+        '''
+
+        :param int:
+        :return: string
+        '''
+
+
+
+
+# ========================================================
+solution = Solution()
+string1 = "12321"
+string2 = "281"
+print solution.string2int(string1)
+print solution.string2int(string2)
